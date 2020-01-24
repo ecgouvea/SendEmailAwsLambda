f6f23c8424672464b76fb9d49e68c832702b9909
EC2 Default User <ec2-user@ip-10-0-0-96.ec2.internal>
2020-01-24 20:41:13 +0000

    first commit

---
+++ SendEmailLambda/SendEmailLambda/index.js
@@ -0,0 +1,36 @@
+var aws = require('aws-sdk');
+var ses = new aws.SES({region: 'us-east-1'});
+
+exports.handler = (event, context, callback) => {
+     var params = {
+        Destination: {
+            ToAddresses: ["eduardo.cgouvea.ext@extrafarma.com.br"]
+        },
+        Message: {
+            Body: {
+                Text: { Data: "Test"
+                    
+                }
+                
+            },
+            
+            Subject: { Data: "Test Email"
+                
+            }
+        },
+        Source: "ecgouvea@hotmail.com"
+    };
+
+    
+     ses.sendEmail(params, function (err, data) {
+        callback(null, {err: err, data: data});
+        if (err) {
+            console.log(err);
+            context.fail(err);
+        } else {
+            
+            console.log(data);
+            context.succeed(event);
+        }
+    });
+};
